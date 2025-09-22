931790d8b111b8cc5628e9a63ee39ac320a7b14d
Mason Baldwin <me.baldwin1016@icloud.com>
2025-09-22 14:39:18 -0400

    Task 4

---
+++ src/Task4_TheaterKiosk.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Task4_TheaterKiosk {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int ageEntry = 0;
+        String newLine = "";
+        String trash = "";
+
+        System.out.println("Enter your age");
+
+        ageEntry = in.nextInt();
+
+        if(ageEntry >= 21)
+        {
+            System.out.println("You get a wrist band");
+        }
+        else
+        {
+
+        }
+
+
+    }
+}
